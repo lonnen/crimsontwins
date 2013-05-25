d4f376356b86caa0de99ccd6164781902c69c001
lonnen <chris.lonnen@gmail.com>
2013-05-25 12:36:10 -0700

    revert

--- test/api.js
+++ test/api.js
@@ -21,7 +21,6 @@ describe('api', function() {
 
   describe('ping', function(done) {
     it('should return pong', function(done) {
-      web.start();
       supertest(web.app)
         .get('/api/ping')
         .expect(200, 'pong', done);
